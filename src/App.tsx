ca8c18bb43df20a3bf156726f953d68dc9a4552b
JulienZaglia <jarjarbinkstobinks@gmail.com>
2022-07-12 10:26:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="pokemon-header">Pokemon</header>
+    </div>
+  );
+}
+
+export default App;
