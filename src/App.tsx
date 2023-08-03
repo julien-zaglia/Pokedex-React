dce25dd6b39a9ea001e9fd503ba376d5847e9ae8
JulienZaglia <jarjarbinkstobinks@gmail.com>
2023-08-03 20:57:27 +0200

    oui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import './App.css';
+import axios from "axios"
+import React, {useEffect,useState} from "react"
+import './App.css'
+import PokemonCollection from "./components/PokemonCollection"
+import { Pokemon } from "./interface"
 
-function App() {
+interface Pokemons {
+  name: string
+  url: string
+}
+
+const App: React.FC = () => {
+
+  const [pokemons, setPokemons] = useState < Pokemon[]>([])
+  const [nextUrl, setNextUrl] = useState <string>("")
+
+  useEffect(() => {
+
+    // déclaration de fonction pour recup les pokemon async car elle va attendre d'avoir le résultat de l'api pour fonctionner 
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      )
+
+      setNextUrl(res.data.next)
+
+      // axios permet de récupérer les données envoyées par l'api plus facilement 
+
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        )
+        setPokemons((p) => [...p, poke.data])
+      })
+    }
+    getPokemon()
+  }, [])
+
   return (
     <div className="App">
-      <header className="pokemon-header">Pokemon</header>
+      <div className="container">
+        <header className="pokemon-header"> Pokemon</header>
+        <PokemonCollection pokemons={pokemons} />
+        {/* <button onClick={nextPage}>Charger</button> */}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
